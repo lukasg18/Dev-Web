72704f0e109dcbcedf06fb85e93293e06fa2efa2
lukasg18 <lukas.gomes2010@gmail.com>
2019-04-23 00:11:50 -0300

    acertando banco

--- tabelasprontas.sql
+++ tabelasprontas.sql
@@ -1,18 +1,18 @@
 INSERT INTO public.jogo(
             nome, anolancamento, urlimagem)
-    VALUES ('Anthem', '22-02-2019', 'https://upload.wikimedia.org/wikipedia/pt/4/49/Cover_Art_of_Anthem.jpg');
+    VALUES ('Anthem', '2019-02-22 00:00:00', 'https://upload.wikimedia.org/wikipedia/pt/4/49/Cover_Art_of_Anthem.jpg');
 
 INSERT INTO public.jogo(
             nome, anolancamento, urlimagem)
-    VALUES ('Destiny', '09-09-2014', 'https://upload.wikimedia.org/wikipedia/pt/thumb/b/be/Destiny_box_art.png/265px-Destiny_box_art.png');
+    VALUES ('Destiny', '2014-09-09', 'https://upload.wikimedia.org/wikipedia/pt/thumb/b/be/Destiny_box_art.png/265px-Destiny_box_art.png');
 
 INSERT INTO public.jogo(
             nome, anolancamento, urlimagem)
-    VALUES ('Mega Man 11', '02-10-2018', 'https://s3.amazonaws.com/comparegame/thumbnails/44056/large.jpg');
+    VALUES ('Mega Man 11', '2018-10-02', 'https://s3.amazonaws.com/comparegame/thumbnails/44056/large.jpg');
 
 INSERT INTO public.jogo(
             nome, anolancamento, urlimagem)
-    VALUES ('Forza Motorsport 6', '15-09-2015', 'https://upload.wikimedia.org/wikipedia/pt/thumb/a/ae/Forza_6_box_art.jpg/230px-Forza_6_box_art.jpg');
+    VALUES ('Forza Motorsport 6', '2015-09-15', 'https://upload.wikimedia.org/wikipedia/pt/thumb/a/ae/Forza_6_box_art.jpg/230px-Forza_6_box_art.jpg');
 
 INSERT INTO public.genero(
              nome)
@@ -48,7 +48,7 @@ INSERT INTO public.plataforma(
 
 INSERT INTO public.jogo_genero(
             idgenero, idjogo)
-    VALUES (1, 3);
+    VALUES (3, 1);
 
 INSERT INTO public.jogo_genero(
             idgenero, idjogo)
@@ -56,15 +56,15 @@ INSERT INTO public.jogo_genero(
 
     INSERT INTO public.jogo_genero(
             idgenero, idjogo)
-    VALUES (2, 3);
+    VALUES (3, 2);
 
     INSERT INTO public.jogo_genero(
             idgenero, idjogo)
-    VALUES (2, 4);
+    VALUES (4, 2);
 
 INSERT INTO public.jogo_genero(
             idgenero, idjogo)
-    VALUES (3, 4);
+    VALUES (4, 3);
 
     INSERT INTO public.jogo_genero(
             idgenero, idjogo)
@@ -76,15 +76,7 @@ INSERT INTO public.jogo_genero(
 
     INSERT INTO public.jogo_genero(
             idgenero, idjogo)
-    VALUES (4, 1);
-
-INSERT INTO public.jogo_plataforma(
-            idplataforma, idjogo)
-    VALUES (1, 2);
-
-INSERT INTO public.jogo_plataforma(
-            idplataforma, idjogo)
-    VALUES (1, 3);
+    VALUES (1, 4);
 
 INSERT INTO public.jogo_plataforma(
             idplataforma, idjogo)
@@ -92,15 +84,15 @@ INSERT INTO public.jogo_plataforma(
 
 INSERT INTO public.jogo_plataforma(
             idplataforma, idjogo)
+    VALUES (3, 1);
+
+INSERT INTO public.jogo_plataforma(
+            idplataforma, idjogo)
+    VALUES (1, 2);
+
+INSERT INTO public.jogo_plataforma(
+            idplataforma, idjogo)
     VALUES (2, 2);
-
-INSERT INTO public.jogo_plataforma(
-            idplataforma, idjogo)
-    VALUES (2, 3);
-
-INSERT INTO public.jogo_plataforma(
-            idplataforma, idjogo)
-    VALUES (2, 4);
 
 INSERT INTO public.jogo_plataforma(
             idplataforma, idjogo)
@@ -108,9 +100,17 @@ INSERT INTO public.jogo_plataforma(
 
 INSERT INTO public.jogo_plataforma(
             idplataforma, idjogo)
+    VALUES (4, 2);
+
+INSERT INTO public.jogo_plataforma(
+            idplataforma, idjogo)
+    VALUES (2, 3);
+
+INSERT INTO public.jogo_plataforma(
+            idplataforma, idjogo)
     VALUES (3, 3);
 
 INSERT INTO public.jogo_plataforma(
             idplataforma, idjogo)
-    VALUES (4, 3);
+    VALUES (3, 4);
 

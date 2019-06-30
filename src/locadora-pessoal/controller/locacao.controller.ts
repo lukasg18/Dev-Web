6d678e1997d008dd20d07e3f39f04adf2c53042a
joseslima <guislima47@gmail.com>
2019-06-30 20:44:05 -0300

    locacaController update

--- src/locadora-pessoal/controller/locacao.controller.ts
+++ src/locadora-pessoal/controller/locacao.controller.ts
@@ -1,49 +1,38 @@
-import { Controller, Post, Body, Res, HttpStatus, Param, Get } from '@nestjs/common';
-import { ApiUseTags, ApiImplicitParam } from '@nestjs/swagger';
+import {
+  Controller,
+  Post,
+  Body,
+  Res,
+  HttpStatus,
+} from '@nestjs/common';
+import { ApiUseTags } from '@nestjs/swagger';
 import { LocacaoService } from '../service/locacao.service';
 import { PagamentoService } from '../service/pagamento.service';
 
-  @ApiUseTags('Locacao')
-  @Controller()
-  export class LocacaoController {
-    constructor(private readonly locacaoService: LocacaoService, private readonly pagamentoService: PagamentoService) {}
-    @Post('/locacao')
-    async createOne(@Res() res, @Body() body: any) {
-      try {
-        const {idcartao, metodopagamento, ...dadosLocacao} = body
-        const locacao = await this.locacaoService.Create(dadosLocacao);
+@ApiUseTags('Locacao')
+@Controller()
+export class LocacaoController {
+  constructor(private readonly locacaoService: LocacaoService, private readonly pagamentoService: PagamentoService) {}
+  @Post('/locacao')
+  async createOne(@Res() res, @Body() body: any) {
+    try {
+      const {idcartao, metodopagamento, ...dadosLocacao} = body
+      const locacao = await this.locacaoService.Create(dadosLocacao);
 
-        if (!locacao) {
-          res
-            .status(HttpStatus.NOT_FOUND)
-            .send('Não foi possível cadastrar');
-        }
-        const pagamento = await this.pagamentoService.Create({
-          tipo: 1, 
-          idcartao, 
-          metodopagamento,
-          modelo: locacao
-        })
-        
-        if (pagamento) {
-          res.status(HttpStatus.OK).send(locacao);
-        } else {
-          res
-            .status(HttpStatus.NOT_FOUND)
-            .send('Não foi possível realizar o pagamento');
-        }
-      } catch (err) {
-        res.status(HttpStatus.BAD_GATEWAY).send(err);
+      if (!locacao) {
+        res
+          .status(HttpStatus.NOT_FOUND)
+          .send('Não foi possível cadastrar');
       }
-      const pagamento = this.pagamentoService.Create({
-        tipo: 1,
-        idcartao,
-        metodoPagamento,
-        modelo: locacao,
-      });
-
+      const pagamento = await this.pagamentoService.Create({
+        tipo: 1, 
+        idcartao, 
+        metodopagamento,
+        modelo: locacao
+      })
+      
       if (pagamento) {
-        res.status(HttpStatus.OK).send('cadastrado com sucesso!');
+        res.status(HttpStatus.OK).send(locacao);
       } else {
         res
           .status(HttpStatus.NOT_FOUND)
@@ -52,27 +41,5 @@ import { PagamentoService } from '../service/pagamento.service';
     } catch (err) {
       res.status(HttpStatus.BAD_GATEWAY).send(err);
     }
-  }
-
-  @Get('/locacao/:idpessoa')
-  @ApiImplicitParam({
-    name: 'idpessoa',
-    description: 'id da pessoa',
-    required: true,
-    type: Number,
-  })
-  async readOne(@Res() res, @Param() idpessoa) {
-    try {
-      let jogo = await this.locacaoService.readUser(idpessoa.idpessoa)
-      if (jogo != undefined) {
-        res.status(HttpStatus.OK).send(jogo);
-      } else {
-        res
-          .status(HttpStatus.NOT_FOUND)
-          .json({ message: 'Nenhum resultado encontrado!' });
-      }
-    } catch (err) {
-      res.status(HttpStatus.BAD_GATEWAY).send(err.message);
-    }
-  }
+  }  
 }
